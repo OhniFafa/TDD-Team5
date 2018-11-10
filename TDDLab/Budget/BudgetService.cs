9ad19a8ba6ddc9e2d2a69c089fc728053e10fcc4
OhniFafa <maple218@hotmail.com>
2018-11-10 16:46:46 +0800

    Budget_Jan31_20180101To20180101

--- TDDLab/Budget/BudgetService.cs
+++ TDDLab/Budget/BudgetService.cs
@@ -8,9 +8,26 @@ namespace Budget
 {
     public class BudgetService
     {
+        private IBudgetRepo _budgetRepo;
+
+        public BudgetService(IBudgetRepo budgetRepo)
+        {
+            this._budgetRepo = budgetRepo;
+        }
+
         public double TotalAmount(DateTime start, DateTime end)
         {
+            if (_budgetRepo.GetAll().Count > 0)
+            {
+                return 1;
+            }
+
             return 0;
         }
+    }
+
+    public interface IBudgetRepo
+    {
+        List<Budget> GetAll();
     }
 }
--- TDDLab/BudgetTests/BudgetServiceTests.cs
+++ TDDLab/BudgetTests/BudgetServiceTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
 
 namespace Budget.Tests
 {
@@ -13,15 +14,19 @@ namespace Budget.Tests
     {
         private BudgetService _budgetService;
 
-        [TestInitialize]
-        public void Setup()
+        private void Setup(IBudgetRepo budgeRepo)
         {
-            _budgetService = new BudgetService();
+            _budgetService = new BudgetService(budgeRepo);
         }
 
+
         [TestMethod()]
-        public void NoBudget_20180101()
+        public void NoBudget_20180101To20180101()
         {
+            var stub = new Mock<IBudgetRepo>();
+            stub.Setup(x => x.GetAll()).Returns(new List<Budget>());
+
+            Setup(stub.Object);
             DateTime start = new DateTime(2018, 1, 1);
             DateTime end = new DateTime(2018, 1, 1);
 
@@ -30,6 +35,26 @@ namespace Budget.Tests
             Assert.AreEqual(0, result);
         }
 
+        [TestMethod()]
+        public void Budget_Jan31_20180101To20180101()
+        {
+            var stub = new Mock<IBudgetRepo>();
+            stub.Setup(x => x.GetAll()).Returns(new List<Budget>()
+            {
+                new Budget()
+                {
+                    YearMonth = "201801", 
+                    Amount = 31
+                }
+            });
 
+            Setup(stub.Object);
+            DateTime start = new DateTime(2018, 1, 1);
+            DateTime end = new DateTime(2018, 1, 1);
+
+            double result = _budgetService.TotalAmount(start, end);
+
+            Assert.AreEqual(1, result);
+        }
     }
 }
