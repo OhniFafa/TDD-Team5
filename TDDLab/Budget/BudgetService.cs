a0c1fb505774ebfc584f35327055bbe5e494d9ea
OhniFafa <maple218@hotmail.com>
2018-11-10 16:26:00 +0800

    First pass

---
+++ TDDLab/Budget/BudgetService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget
+{
+    public class BudgetService
+    {
+        public double TotalAmount(DateTime start, DateTime end)
+        {
+            return 0;
+        }
+    }
+}
---
+++ TDDLab/BudgetTests/BudgetServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Budget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget.Tests
+{
+    [TestClass()]
+    public class BudgetServiceTests
+    {
+        private BudgetService _budgetService;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _budgetService = new BudgetService();
+        }
+
+        [TestMethod()]
+        public void NoBudget_20180101()
+        {
+            DateTime start = new DateTime(2018, 1, 1);
+            DateTime end = new DateTime(2018, 1, 1);
+
+            double result = _budgetService.TotalAmount(start, end);
+
+            Assert.AreEqual(0, result);
+        }
+
+
+    }
+}
